82a5f90d430e78a52b2523a29048559df2f453d4
shengyajie <13733858073@163.com>
2020-10-24 04:50:20 +0800

    la yu ilayui实现前后端分离

--- web/views/js/index.js
+++ web/views/js/index.js
@@ -4,9 +4,9 @@ $(function() {
         datatype: "json", //数据类型
         url: '../../../api_server/db/data.json',
         success: function(res) {
-            // console.log(res.result.data)
+            console.log(res.result.data)
             var htmlStr = template('tinyA', res.result);
-            $('.tiny_img').html(htmlStr)
+            $('.TiB').html(htmlStr)
         }
     })
 })
