319a50d02403d58f89867b874ce0a723a9de58d4
shengyajie <13733858073@163.com>
2020-10-24 00:17:14 +0800

    分类

---
+++ web/views/js/index.js
@@ -0,0 +1,12 @@
+$(function() {
+    $.ajax({
+        method: 'get',
+        datatype: "json", //数据类型
+        url: '../../../api_server/db/data.json',
+        success: function(res) {
+            // console.log(res.result.data)
+            var htmlStr = template('tinyA', res.result);
+            $('.tiny_img').html(htmlStr)
+        }
+    })
+})
